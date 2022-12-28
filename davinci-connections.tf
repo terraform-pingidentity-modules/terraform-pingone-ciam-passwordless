e7d967ff6f3196cf28ee4fa57c0b799de0445260
Terry Sigle <terrysigle@pingidentity.com>
2022-12-28 12:34:00 -0600

    Automated update from Package Packer

--- davinci-connections.tf
+++ davinci-connections.tf
@@ -1,36 +1,36 @@
 
 resource "davinci_connection" "flowConnector" {
-  name           = "flowConnector"
+  name           = "undefined"
   connector_id   = "flowConnector"
   environment_id = resource.pingone_environment.my_environment.id
 }
 
 resource "davinci_connection" "nodeConnector" {
-  name           = "nodeConnector"
+  name           = "undefined"
   connector_id   = "nodeConnector"
   environment_id = resource.pingone_environment.my_environment.id
 }
 
 resource "davinci_connection" "pingOneMfaConnector" {
-  name           = "pingOneMfaConnector"
+  name           = "undefined"
   connector_id   = "pingOneMfaConnector"
   environment_id = resource.pingone_environment.my_environment.id
 }
 
 resource "davinci_connection" "challengeConnector" {
-  name           = "challengeConnector"
+  name           = "undefined"
   connector_id   = "challengeConnector"
   environment_id = resource.pingone_environment.my_environment.id
 }
 
 resource "davinci_connection" "notificationsConnector" {
-  name           = "notificationsConnector"
+  name           = "undefined"
   connector_id   = "notificationsConnector"
   environment_id = resource.pingone_environment.my_environment.id
 }
 
 resource "davinci_connection" "devicePolicyConnector" {
-  name           = "devicePolicyConnector"
+  name           = "undefined"
   connector_id   = "devicePolicyConnector"
   environment_id = resource.pingone_environment.my_environment.id
 }
